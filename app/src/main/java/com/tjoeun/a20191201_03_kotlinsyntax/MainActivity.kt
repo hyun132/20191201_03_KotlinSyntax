21208fcd7c5dc44970e319c9e0902f11b23f6a37
미현 김 <rlaalgus132@naver.com>
2019-12-01 16:00:05 +0900

    배열안에 있는 데이터 하나하나 출력

--- app/src/main/java/com/tjoeun/a20191201_03_kotlinsyntax/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191201_03_kotlinsyntax/MainActivity.kt
@@ -46,6 +46,12 @@ class MainActivity : AppCompatActivity() {
 
         userList.add(user2)
 
+        val user4=User()
+        user2.name="김미현"
+        user2.loginId="student2"
+
+        userList.add(user4)
+
         Log.d("들어있는 사람 수 ", "${userList.size} 명")
         Log.d("1번칸에 들어있는 사람 이름", "${userList.get(1).name}")
 
@@ -53,8 +59,26 @@ class MainActivity : AppCompatActivity() {
         user3.name="조경진"
         user3.loginId="ch"
 
-        Log.d("조경진이 있는 위치 ", "${userList.indexOf(user3)}")
-        userList.remove(user3)
-        Log.d("들어있는 사람 수","${userList.size} 명" )
+//        Log.d("조경진이 있는 위치 ", "${userList.indexOf(user3)}")
+//        userList.remove(user3)
+        Log.d("들어있는 사람 수","${userList.size} 명")
+
+
+
+//        var count=0
+//        while(true){
+//            count ++
+//
+//            if (count >= 10){
+//                break
+//            }
+//
+//        }
+
+//        사용자 목록에 들어있는 사람의 이름 / 아이디 출력
+
+        for (user in userList){
+            Log.d("사람이름/아이디","${user.name} / ${user.loginId}")
+        }
     }
 }
