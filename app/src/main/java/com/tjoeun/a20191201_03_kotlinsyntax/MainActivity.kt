78b4d5eb3cd01efae8342174eba394d8be2c61a5
미현 김 <rlaalgus132@naver.com>
2019-12-01 15:33:33 +0900

    숫자 다루는 ArrayList 활용 연습

--- app/src/main/java/com/tjoeun/a20191201_03_kotlinsyntax/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191201_03_kotlinsyntax/MainActivity.kt
@@ -2,6 +2,7 @@ package com.tjoeun.a20191201_03_kotlinsyntax
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +10,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        
+        val lottoNumArr = ArrayList<Int>()
+
+        lottoNumArr.add(10)
+        lottoNumArr.add(42)
+        lottoNumArr.add(0,15)
+        lottoNumArr.add(17)
+
+
+        Log.d("로또번호 입력한 갯수", "${lottoNumArr.size} 개 입력됨")
+
+        lottoNumArr.removeAt(0)
+
+        Log.d("42는 몇번 칸에?" ,"${lottoNumArr.indexOf(42)} 칸에 있음")
+
+        Log.d("1번칸에는 뭐가 들어있나?","${lottoNumArr.get(1)}")
+
+//        42를 배열에서 빼내고싶다.
+        lottoNumArr.remove(42)
     }
 }
