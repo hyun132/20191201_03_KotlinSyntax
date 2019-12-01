a138ae211d43613cb41805190fe3721c44fc1a22
미현 김 <rlaalgus132@naver.com>
2019-12-01 15:02:11 +0900

    Initial commit

---
+++ app/src/main/java/com/tjoeun/a20191201_03_kotlinsyntax/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.tjoeun.a20191201_03_kotlinsyntax
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        
+    }
+}
